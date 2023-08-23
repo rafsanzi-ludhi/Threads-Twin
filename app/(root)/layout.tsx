a4aec4b163ea9c66b8d49dbd02f84a651f552c06
rafalos99 <rafsanzaniludhi@gmail.com>
2023-08-23 17:53:26 +0100

    topbar styling. logo needed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,12 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from "@clerk/nextjs";
 
 
+import Topbar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Bottombar from '@/components/shared/Bottombar';
+
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
