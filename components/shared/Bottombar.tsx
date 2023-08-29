6ef5ac44b45ee4569245b913034b53d0f8341543
rafalos99 <rafsanzaniludhi@gmail.com>
2023-08-29 11:50:41 +0100

    bottombar links added. adjusted for smaller screen

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,5 +1,47 @@
+"use client"
+
+
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
 function Bottombar() {
-    return <h1>Bottombar</h1>
+    const pathname = usePathname();
+
+        return (
+        <section className="bottombar">
+            <div className="bottombar_container">
+                    {sidebarLinks.map((link) => {
+                        // is Active variable ---> also needs styling 
+                        const isActive = (pathname.includes
+                        (link.route) && link.route.length >
+                        1) || pathname === link.route;            
+
+                
+                     return (
+                        <Link
+                            href={link.route}
+                            key={link.label}
+                            className={`bottombar_link ${isActive && "bg-primary-500 "}`}
+                        >
+                            <Image
+                                src={link.imgURL}
+                                alt={link.label}
+                                width={24}
+                                height={24}
+                            />
+                            {/* show labels around tablet widths only and hide below 640px*/}
+                            <p className="text-subtle-medium text-light-1 max-sm:hidden">{link.label.split(/\s+/)[0]}</p>
+                        </Link>
+                    )
+                }
+                )}
+
+            </div>
+
+        </section>
+    )
 }
 
 export default Bottombar;
