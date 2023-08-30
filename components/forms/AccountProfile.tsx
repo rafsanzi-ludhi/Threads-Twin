815f7942ea88663f6ddeb1c752a8de7392f16c93
rafalos99 <rafsanzaniludhi@gmail.com>
2023-08-31 00:07:50 +0100

    onboarding added username and bio

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -12,6 +12,8 @@ import {
     FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UserValidation } from "@/lib/validations/user";
 import * as z from "zod"
@@ -96,6 +98,66 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                     </FormItem>
                 )}
                 />
+
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                    <FormItem className="flex items-center gap-3 w-full">
+                        <FormLabel className="text-base-semibold text-light-2">
+                            Name
+                        </FormLabel>
+                        <FormControl className="flex-1 text-base-semibold text-grey-200">
+                                <Input
+                                    type="text"
+                                    className="account-form_input no-focus"
+                                    {...field}
+                            />
+                        </FormControl>
+                    </FormItem>
+                )}
+                />
+
+
+                <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                    <FormItem className="flex items-center gap-3 w-full">
+                        <FormLabel className="text-base-semibold text-light-2">
+                            Username
+                        </FormLabel>
+                        <FormControl className="flex-1 text-base-semibold text-grey-200">
+                                <Input
+                                    type="text"
+                                    className="account-form_input no-focus"
+                                    {...field}
+                            />
+                        </FormControl>
+                    </FormItem>
+                )}
+                />
+                
+                
+                <FormField
+                    control={form.control}
+                    name="bio"
+                    render={({ field }) => (
+                    <FormItem className="flex items-center gap-3 w-full">
+                        <FormLabel className="text-base-semibold text-light-2">
+                            Bio
+                        </FormLabel>
+                        <FormControl className="flex-1 text-base-semibold text-grey-200">
+                                <Textarea
+                                    rows={10}
+                                    className="account-form_input no-focus"
+                                    {...field}
+                            />
+                        </FormControl>
+                    </FormItem>
+                )}
+                />
+                 
                 <Button type="submit" className="lightblue">Submit</Button>
             </form>
         </Form>
