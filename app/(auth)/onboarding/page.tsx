1c33117bb4f102e6812f83abaff236c3b6da2416
rafalos99 <rafsanzaniludhi@gmail.com>
2023-08-30 19:56:42 +0100

    update omboarding middelware

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,21 +1,18 @@
 import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
 
-
 async function Page() {
     const user = await currentUser();
-
-    const userInfo = await {};
+    const userInfo = {};
 
     const userData = {
         id: user?.id,
         objectId: userInfo?._id,
-        username: userInfo ? userInfo?.username || user.username,
-        name: userInfo ? userInfo?.name || user.firstName ?? "",
-        bio: userInfo ? userInfo?.bio || "",
-        image: userInfo ? userInfo?.image || user?.imageUrl,
+        username: userInfo?.username ?? user?.username,
+        name: userInfo?.name ?? user?.firstName ?? "",
+        bio: userInfo?.bio ?? "",
+        image: userInfo?.image ?? user?.imageUrl,
     };
-
 
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
