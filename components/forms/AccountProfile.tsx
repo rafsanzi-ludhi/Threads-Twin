5a91f8ba511d78c4c29904809e846ddba6cd7463
rafalos99 <rafsanzaniludhi@gmail.com>
2023-08-31 16:16:11 +0100

    upload thing, core.ts and route.ts are the api endpoints

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,6 +19,8 @@ import { UserValidation } from "@/lib/validations/user";
 import * as z from "zod"
 import Image from "next/image";
 import { ChangeEvent, useState } from "react";
+import { isBase64Image } from "@/lib/utils";
+import ( useUploadThing ) from "@/lib/uploadthing";
 
 interface Props {
   user: {
@@ -70,9 +72,13 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     
     
     function onSubmit(values: z.infer<typeof UserValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
+        const blob = values.profile_photo;
+
+        const hasImageChanged = isBase64Image(blob);
+
+        if (hasImageChanged) {
+            const imgRes = 
+        }
     }
 
     return (
