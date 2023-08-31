2a40dfaeb0dcbdabd562f410241214418f30b7f0
rafalos99 <rafsanzaniludhi@gmail.com>
2023-08-31 16:47:16 +0100

    Account profile data to be sent to backend

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,7 +20,7 @@ import * as z from "zod"
 import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { isBase64Image } from "@/lib/utils";
-import ( useUploadThing ) from "@/lib/uploadthing";
+import { useUploadThing } from "@/lib/uploadthing";
 
 interface Props {
   user: {
@@ -36,6 +36,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([])
+    const { startUpload } = useUploadThing("media");
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -71,14 +72,20 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     }
     
     
-    function onSubmit(values: z.infer<typeof UserValidation>) {
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
         const blob = values.profile_photo;
 
         const hasImageChanged = isBase64Image(blob);
 
         if (hasImageChanged) {
-            const imgRes = 
+            const imgRes = await startUpload(files); 
+
+            if (imgRes && imgRes[0].fileUrl) {
+                values.profile_photo = imgRes[0].fileUrl;
+            } 
         }
+
+        //TODO: Call a backend function to update user profile
     }
 
     return (
