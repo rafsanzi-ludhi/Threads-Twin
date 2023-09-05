9e41a6dde9d2ffb365ca9019888c89df101970e7
rafalos99 <rafsanzaniludhi@gmail.com>
2023-09-05 21:36:40 +0100

    ux design of main pages. Added in ThreadsTab

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,47 +1,45 @@
-"use client"
+"use client";
 
-
-import { sidebarLinks } from "@/constants";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+import { sidebarLinks } from "@/constants";
+
 function Bottombar() {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-        return (
-        <section className="bottombar">
-            <div className="bottombar_container">
-                    {sidebarLinks.map((link) => {
-                        // is Active variable ---> also needs styling 
-                        const isActive = (pathname.includes
-                        (link.route) && link.route.length >
-                        1) || pathname === link.route;            
+  return (
+    <section className='bottombar'>
+      <div className='bottombar_container'>
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
 
-                
-                     return (
-                        <Link
-                            href={link.route}
-                            key={link.label}
-                            className={`bottombar_link ${isActive && "bg-primary-500 "}`}
-                        >
-                            <Image
-                                src={link.imgURL}
-                                alt={link.label}
-                                width={24}
-                                height={24}
-                            />
-                            {/* show labels around tablet widths only and hide below 640px*/}
-                            <p className="text-subtle-medium text-light-1 max-sm:hidden">{link.label.split(/\s+/)[0]}</p>
-                        </Link>
-                    )
-                }
-                )}
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={16}
+                height={16}
+                className='object-contain'
+              />
 
-            </div>
-
-        </section>
-    )
+              <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Bottombar;
--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,66 +1,65 @@
-"use client"
+"use client";
 
-import { sidebarLinks } from "@/constants"
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { OrganizationSwitcher, SignedIn } from "@clerk/nextjs";
-import { SignOutButton } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 
+import { sidebarLinks } from "@/constants";
 
-function LeftSidebar() {
-    // see if link is active by showing us which url we are on
-    const router = useRouter(); //make sure to add a declaration at the top of the file "use client"
-    const pathname = usePathname();
-    
-    return (
-        <section className="custom-scrollbar leftsidebar">
-            <div className="flex w-full flex-1 flex-col gap-6 px-6">
-                {sidebarLinks.map((link) => {
-                    // is Active variable ---> also needs styling 
-                    const isActive = (pathname.includes
-                        (link.route) && link.route.length >
-                        1) || pathname === link.route;            
+const LeftSidebar = () => {
+  const router = useRouter();
+  const pathname = usePathname();
 
-                
-                    return (
-                        <Link
-                            href={link.route}
-                            key={link.label}
-                            className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}
-                        >
-                            <Image
-                                src={link.imgURL}
-                                alt={link.label}
-                                width={24}
-                                height={24}
-                            />
-                            {/* hide label at smaller widths below 1024px*/}
-                            <p className="text-light-1 max-lg:hidden">{link.label}</p>
-                        </Link>
-                    )
-                }
-                )}
+  const { userId } = useAuth();
+
+  return (
+    <section className='custom-scrollbar leftsidebar'>
+      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+
+              <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className='mt-10 px-6'>
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+            <div className='flex cursor-pointer gap-4 p-4'>
+              <Image
+                src='/assets/logout.svg'
+                alt='logout'
+                width={24}
+                height={24}
+              />
+
+              <p className='text-light-2 max-lg:hidden'>Logout</p>
             </div>
-
-            <div className="mt-10 px-6">
-                <SignedIn>
-                    <SignOutButton signOutCallback={() => router.push('/sign-in')}>
-                        <div className='flex cursor-pointer gap-4 p-4'>
-                            <Image
-                            src='/public/assets/logout.svg'
-                            alt='logout'
-                            width={24}
-                            height={24}
-                            />
-                            
-                            <p className="text-light-2 max-lg:hidden">Logout</p>
-                        </div>
-                    </SignOutButton>
-                </SignedIn>
-            </div>
-        </section>
-    )
-}
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
+  );
+};
 
 export default LeftSidebar;
